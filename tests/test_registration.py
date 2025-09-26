711d95b2fa1e3bf0719ab828c58ecd3b5fe11eaa
Dmitry Varchak <dmitvar@gmail.com>
2025-09-26 10:15:25 +0300

    7.1 Знакомство с PageObject. Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,32 +1,19 @@
 import pytest
 
-from playwright.sync_api import Page, expect
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
-    chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user@gmail.com')
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    registration_page.fill_registration_form(
+        email='new_user@gmail.com',
+        username='new_user',
+        password='password'
+    )
+    registration_page.click_registration_button()
 
-    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-
-    #     context.storage_state(path='browser-state.json')
-    #
-    # with sync_playwright() as playwright:
-    #     browser = playwright.chromium.launch(headless=False)
-    #     context = browser.new_context(storage_state='browser-state.json')
-    #     page = context.new_page()
-    #
-    #     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-    #
-    #     page.wait_for_timeout(5000)
+    dashboard_page.check_visible_dashboard_toolbar_title()
