de53150fed0b05bb766ab69ad2c940291cbb0a0d
Dmitry Varchak <dmitvar@gmail.com>
2025-09-29 18:12:19 +0300

    7.7 Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- components/courses/course_view_menu_componet.py
+++ components/courses/course_view_menu_componet.py
@@ -8,7 +8,7 @@ class CourseViewMenuComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
         self.menu_button = Button(page, 'course-view-menu-button', 'Menu')
-        self.edit_menu_button =Button(page,'course-view-menu-button', 'Edit')
+        self.edit_menu_button = Button(page,'course-view-edit-menu-item', 'Edit')
         self.delete_menu_button = Button(page, 'course-view-delete-menu-item', 'Delete')
 
     def click_edit(self, index: int):
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,52 +1,89 @@
 import pytest
 from fixtures.pages import create_course_page, courses_list_page
 
-
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(courses_list_page: CoursesListPage):
-    courses_list_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+class TestCourses:
+    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
+        courses_list_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-    courses_list_page.navbar.check_visible('username')
-    courses_list_page.sidebar.check_visible()
+        courses_list_page.navbar.check_visible('username')
+        courses_list_page.sidebar.check_visible()
 
-    courses_list_page.toolbar_view.check_visible()
-    courses_list_page.check_visible_empty_view()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.check_visible_empty_view()
 
+    def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
 
-def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-    create_course_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.create_course_toolbar.check_visible()
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+        create_course_page.create_course_form.check_visible(
+            title='',
+            description='',
+            estimated_time='',
+            max_score='0',
+            min_score='0'
+        )
 
-    create_course_page.create_course_toolbar.check_visible()
-    create_course_page.image_upload_widget.check_visible( is_image_uploaded=False)
-    create_course_page.create_course_form.check_visible(
-        title='',
-        description='',
-        estimated_time='',
-        max_score='0',
-        min_score='0'
-    )
-    create_course_page.create_exercise_toolbar.check_visible()
-    create_course_page.check_visible_exercises_empty_view()
-    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
-    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.create_course_form.fill(
-        title='Playwright',
-        estimated_time='2 weeks',
-        description='Playwright',
-        max_score='100',
-        min_score='10'
-    )
-    create_course_page.create_course_toolbar.click_create_course_button()
-    courses_list_page.toolbar_view.check_visible()
-    courses_list_page.course_view.check_visible(
-        index=0,
-        title='Playwright',
-        estimated_time='2 weeks',
-        max_score='100',
-        min_score='10'
-    )
+        create_course_page.create_exercise_toolbar.check_visible()
+        create_course_page.check_visible_exercises_empty_view()
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title='Playwright',
+            estimated_time='2 weeks',
+            description='Playwright',
+            max_score='100',
+            min_score='10'
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Playwright',
+            estimated_time='2 weeks',
+            max_score='100',
+            min_score='10'
+        )
+
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.create_course_form.fill(
+            title='Playwright',
+            estimated_time='2 weeks',
+            description='Playwright',
+            max_score='100',
+            min_score='10'
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Playwright',
+            estimated_time='2 weeks',
+            max_score='100',
+            min_score='10'
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(
+            title='Playwright turbo',
+            estimated_time='1 weeks',
+            description='Playwright turbo',
+            max_score='500',
+            min_score='50'
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Playwright turbo',
+            estimated_time='1 weeks',
+            max_score='500',
+            min_score='50'
+        )
