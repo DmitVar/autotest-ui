2c25a19ee90fcdb70eff9f946aa35ac835f29d5f
Dmitry Varchak <dmitvar@gmail.com>
2025-10-01 17:44:04 +0300

    8.2 Интеграция Allure в автотесты.Практическая работа с Allure аннотациями

--- elements/file_input.py
+++ elements/file_input.py
@@ -1,7 +1,14 @@
+import allure
+
 from elements.base_element import BaseElement
 
 
 class FileInput(BaseElement):
+    @property
+    def type_of(self)->str:
+        return 'file'
+
     def set_input_files(self, file: str,  nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        locator.set_input_files(file)
+        with allure.step(f'Setting {self.type_of} "{self.name}" to {file}'):
+            locator = self.get_locator(nth, **kwargs)
+            locator.set_input_files(file)
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,19 @@
 from typing import Pattern
 from playwright.sync_api import Page, expect
+import allure
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
     def vizit(self, url: str) -> None:
-        self.page.goto(url, wait_until="networkidle")
+        with allure.step((f'Open the url: "{url}"')):
+            self.page.goto(url, wait_until="networkidle")
 
     def reload(self) -> None:
-        self.page.reload(wait_until="domcontentloaded")
+        with allure.step(f'Reload page with url: "{self.page.url}"'):
+            self.page.reload(wait_until="domcontentloaded")
 
     def check_current_url(self, expected_url: Pattern[str]):
-        expect(self.page).to_have_url(expected_url)
+        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+            expect(self.page).to_have_url(expected_url)
