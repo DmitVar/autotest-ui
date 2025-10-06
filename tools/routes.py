7df3e9051a3b912b663fdaf7d09ce3a56f3300be
Dmitry Varchak <dmitvar@gmail.com>
2025-10-06 19:30:31 +0300

    10.4 Запуск автотестов на CI изменена фикс1

--- tools/routes.py
+++ tools/routes.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class AppRoute(str, Enum):
+    """
+    ENUM содержит перечень адресов запросов
+    """
     LOGIN = "./#/auth/login"
     REGISTRATION = "./#/auth/registration"
     DASHBOARD = "./#/dashboard"
