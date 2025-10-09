651785fcea6a6ca59253bab1052ff06b436895a4
Dmitry Varchak <dmitvar@gmail.com>
2025-10-09 18:14:54 +0300

    13.1 Итоговый проект.

--- tests/dasboard/test_dashboard.py
+++ tests/dasboard/test_dashboard.py
@@ -32,6 +32,7 @@ class TestDashboard:
         dashboard_page_with_state.toolbar.check_visible()
         dashboard_page_with_state.scores_chart_view.check_visible('Scores')
         dashboard_page_with_state.courses_chart_view.check_visible('Courses')
+
         dashboard_page_with_state.activities_chart_view.check_visible('Activities')
         dashboard_page_with_state.students_chart_view.check_visible('Students')
         dashboard_page_with_state.navbar.check_visible(username=settings.test_user.username)
